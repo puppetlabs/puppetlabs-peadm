7afb85ee5e82c390df235f245bca6029699c2973
Reid Vandewiele <reid@puppetlabs.com>
2018-06-26 10:42:49 -0700

    Add file_content_upload function
    To allow templatized files to be created and uploaded using tasks

---
+++ lib/puppet/functions/pe_xl/file_content_upload.rb
@@ -0,0 +1,23 @@
+require 'tempfile'
+
+Puppet::Functions.create_function(:'pe_xl::file_content_upload') do
+  local_types do
+    type 'TargetOrTargets = Variant[String[1], Target, Array[TargetOrTargets]]'
+  end
+
+  dispatch :file_content_upload do
+    param 'String[1]', :content
+    param 'String[1]', :destination
+    repeated_param 'TargetOrTargets', :targets
+  end
+
+  def file_content_upload(content, destination, *targets)
+    file = Tempfile.new('pe_xl')
+    file.write(content)
+    file.flush
+    result = call_function('file_upload', file.path, destination, *targets)
+    file.close
+    file.unlink
+    result
+  end
+end
