3d79ebe4c7781418fe39b4d37f3f06ee32c17afb
Reid Vandewiele <reid@puppetlabs.com>
2018-06-26 12:53:26 -0700

    Add missing params to plan

--- plans/install.pp
+++ plans/install.pp
@@ -61,6 +61,8 @@ plan pe_xl::install (
 
   pe_xl::file_content_upload($primary_master_pe_conf, '/tmp/pe.conf', $primary_master_host)
   run_task('pe_xl::pe_install', $primary_master_host,
+    tarball             => $pe_tarball,
+    peconf              => '/tmp/pe.conf',
     csr_attributes_yaml => @("HEREDOC")
       ---
       extension_requests:
@@ -71,6 +73,8 @@ plan pe_xl::install (
 
   pe_xl::file_content_upload($puppetdb_database_pe_conf, '/tmp/pe.conf', $puppetdb_database_host)
   run_task('pe_xl::pe_install', $puppetdb_database_host,
+    tarball             => $pe_tarball,
+    peconf              => '/tmp/pe.conf',
     csr_attributes_yaml => @("HEREDOC")
       ---
       extension_requests:
