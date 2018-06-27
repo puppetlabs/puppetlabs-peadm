2b3c10404e4e755f04ab80f8b3585c34be5531b0
Reid Vandewiele <reid@puppetlabs.com>
2018-06-27 16:43:02 -0700

    Make download smarter

--- tasks/pe_download.sh
+++ tasks/pe_download.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-download() {
-  curl -o "${PT_filename}" "https://s3.amazonaws.com/pe-builds/released/${PT_version}/puppet-enterprise-${PT_version}-el-7-x86_64.tar.gz"
-}
+uri="https://s3.amazonaws.com/pe-builds/released/${PT_version}/puppet-enterprise-${PT_version}-el-7-x86_64.tar.gz"
+urisize=$(curl -s --head $uri | sed -n -e 's/^M//' -e 's/Content-Length: \([0-9]\+\)/\1/p')
+filesize=$(stat -c%s "$PT_filename" 2>/dev/null)
 
-# Make this assume that if the file exists and is of a certain minimum size
+# Assume that if the file exists and is of a certain minimum size
 # it's probably good enough to use and don't have to re-download.
-acceptsize=400000000
-if [ ! -e "${PT_filename}" ]; then
-  download
-elif [ ! $(stat -c%s "$PT_filename") -gt "$acceptsize" ]; then
-  download
+if [ "$filesize" -ne "$urisize" ]; then
+  curl -o "$PT_filename" "$uri"
 else
   exit 0
 fi
