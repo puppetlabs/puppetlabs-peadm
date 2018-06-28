42186576d6a5b2e262626bf4fbabe0329125be55
Reid Vandewiele <reid@puppetlabs.com>
2018-06-28 13:03:18 -0700

    fix bug in agent_install

--- tasks/agent_install.sh
+++ tasks/agent_install.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-flags=$(echo $PT_install_flags | tr -d '[],')
+flags=$(echo $PT_install_flags | tr -d '[],"')
 curl -k "https://${PT_server}:8140/packages/current/install.bash" | bash -s -- $flags
