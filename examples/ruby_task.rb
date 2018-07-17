16cf78dbb36327103d15e2ff3deeba70f07eba1d
Reid Vandewiele <reid@puppetlabs.com>
2018-07-16 17:22:45 -0700

    Store example ruby task somewhere
    For use later as a template

---
+++ examples/ruby_task.rb
@@ -0,0 +1,28 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+require 'json'
+require 'logger'
+require 'open3'
+
+# Parameters expected:
+#   Hash
+#     String mom_host
+#     String database_host
+#     Boolean debug
+$params = JSON.parse(STDIN.read)
+
+$logger = Logger.new(STDOUT)
+$logger.level = $params['debug'] ? Logger::DEBUG : Logger::INFO
+
+def main
+end
+
+def run_command(*command)
+  stdout, status = Open3.capture2(*command)
+  unless status.success?
+    @logger.error "while running command: #{command}"
+    @logger.error stdout
+  end
+  stdout
+end
+
+main
