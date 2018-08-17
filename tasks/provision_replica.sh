20b203448d3ea0131956938a53f438ca5370968f
root <root@pe-xl-core-0.cfr.puppet.vm>
2018-08-17 21:04:22 +0000

    added a check for exit 1 which is not worth failing config for

--- tasks/provision_replica.sh
+++ tasks/provision_replica.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e
-
 env PATH=/opt/puppetlabs/bin:$PATH puppet infrastructure provision replica $PT_primary_master_replica
 
+EXIT=$?
+
+if [[ $EXIT == '1' ]]; then
+  echo 'Provision exitted with a code 1, NOT worth stopping for. Continuing.'
+  exit 0
+fi
+
+exit $EXIT
