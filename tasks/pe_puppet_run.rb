c5b36336cd701295f170b0db1e2a9b64c6d901ac
root <root@pe-xl-core-0.cfr.puppet.vm>
2018-08-20 19:00:10 +0000

    added ruby version of puppet run for better use in pxp-agent

---
+++ tasks/pe_puppet_run.rb
@@ -0,0 +1,26 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'json'
+require 'open3'
+
+def get2(command)
+  output, exit_code = Open3.popen2({}, command, err: [:child, :out]) do |_i, o, w|
+    out = o.read
+    exit_code = w.value.exitstatus
+    [out, exit_code]
+  end
+
+  { _output: output,
+    exit_code: exit_code }
+end
+
+command = 'env PATH=/opt/puppetlabs/bin:$PATH puppet agent --onetime   --verbose \
+  --no-daemonize --no-usecacheonfailure --no-splay --no-use_cached_catalog'
+
+begin
+  result = get2(command)
+  puts result.to_json
+
+  exit result[:exit_code]
+end
+
