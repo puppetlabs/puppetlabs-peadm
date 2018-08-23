05d1c354fbbe8abd822494475cec966abe663f7e
root <root@pe-xl-core-0.cfr.puppet.vm>
2018-08-23 12:56:43 +0000

    new ruby downloader

---
+++ tasks/download.rb
@@ -0,0 +1,34 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+#
+# Puppet task for downloading a file from network to target location
+#
+# Parameters:
+#   * source - The target location (http/https) to download the file from
+#   * path - The location to save the file
+#
+require 'open3'
+
+Puppet.initialize_settings
+
+def exports(source, path)
+  stdout, stderr, status = Open3.capture3('/opt/puppetlabs/puppet/bin/curl', '-k', '-o', path, source)
+  {
+      stdout: stdout.strip,
+      stderr: stderr.strip,
+      exit_code: status.exitstatus,
+  }
+
+end
+
+params = JSON.parse(STDIN.read)
+#path = ENV['PT_path']
+#source = ENV['PT_source']
+path = params['path']
+source = params['source']
+
+output = download(source, path)
+if output[:exit_code].zero?
+  puts output[:stdout]
+else
+  puts "There was a problem: #{output[:stderr]}"
+end
