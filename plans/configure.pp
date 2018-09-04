453ca21f0a79d6a5d7936207909ab2f94500ac45
root <root@pe-xl-core-0.cfr.puppet.vm>
2018-09-04 19:18:54 +0000

    added ability to use "local://" transport for primary_master

--- plans/configure.pp
+++ plans/configure.pp
@@ -9,6 +9,7 @@ plan pe_xl::configure (
   Array[String[1]]    $compile_master_hosts = [ ],
   Array[String[1]]    $dns_alt_names = [ ],
 
+  Optional[Boolean]   $run_from_primary = true,
   Optional[String[1]] $primary_master_replica_host = undef,
   Optional[String[1]] $puppetdb_database_replica_host = undef,
 
@@ -19,9 +20,18 @@ plan pe_xl::configure (
   String[1]           $stagingdir = '/tmp',
 ) {
 
+  if $run_from_primary {
+    # set transport for primary_master_host to local
+    $primary_master_host_local = [$primary_master_host].map |$n| { "local://${n}" }
+  } else {
+    # leave default transport for primary_master_host to local
+    $primary_master_host_local = $primary_master_host
+  }
+  
+
   # Set variables and groups of nodes
   $all_hosts_pe_hosts = [
-    $primary_master_host,
+    $primary_master_host_local,
     $puppetdb_database_host,
     $primary_master_replica_host,
     $puppetdb_database_replica_host,
@@ -39,7 +49,7 @@ plan pe_xl::configure (
 
   # This will see if any content is in the r10k_sources hash
   if $control_repo {
-    run_task('pe_xl::code_manager', $primary_master_host,
+    run_task('pe_xl::code_manager', $primary_master_host_local,
       action => 'deploy',
       environment => $deploy_environment,
     )
@@ -49,7 +59,7 @@ plan pe_xl::configure (
     $nm_module_tarball = 'WhatsARanjit-node_manager-0.7.1.tar.gz'
   
     # Download module tar files
-    run_task('pe_xl::download', $primary_master_host,
+    run_task('pe_xl::download', $primary_master_host_local,
       source => "https://forge.puppet.com/v3/files/${nm_module_tarball}", 
       path   => "/tmp/${nm_module_tarball}",
     )
@@ -57,40 +67,40 @@ plan pe_xl::configure (
     $pexl_module_tarball = 'reidmv-pe_xl-master.tar.gz'
   
     # Download module tar files
-    run_task('pe_xl::download', $primary_master_host,
+    run_task('pe_xl::download', $primary_master_host_local,
       source => 'https://github.com/reidmv/reidmv-pe_xl/archive/master.tar.gz', 
       path   => "/tmp/${pexl_module_tarball}",
     )
   
-    run_command("/opt/puppetlabs/bin/puppet module install /tmp/${nm_module_tarball}", $primary_master_host)
-    run_command("/opt/puppetlabs/bin/puppet module install /tmp/${pexl_module_tarball}", $primary_master_host)
-    run_command('chown -R pe-puppet:pe-puppet /etc/puppetlabs/code', $primary_master_host)
+    run_command("/opt/puppetlabs/bin/puppet module install /tmp/${nm_module_tarball}", $primary_master_host_local)
+    run_command("/opt/puppetlabs/bin/puppet module install /tmp/${pexl_module_tarball}", $primary_master_host_local)
+    run_command('chown -R pe-puppet:pe-puppet /etc/puppetlabs/code', $primary_master_host_local)
   }
 
-  run_task('pe_xl::configure_node_groups', $primary_master_host,
+  run_task('pe_xl::configure_node_groups', $primary_master_host_local,
     primary_master_host  => $primary_master_host,
     load_balancer_host   => $load_balancer_host,
     compile_master_hosts => $compile_master_hosts,
     r10k_sources         => $r10k_sources,
   )
 
-  run_task('pe_xl::pe_puppet_run', $primary_master_host)
+  run_task('pe_xl::pe_puppet_run', $primary_master_host_local)
   run_task('pe_xl::pe_puppet_run', $puppetdb_database_host)
   run_task('pe_xl::pe_puppet_run', $primary_master_replica_host)
   run_task('pe_xl::pe_puppet_run', $puppetdb_database_replica_host)
 
   # Run the PE Replica Provision
-  run_task('pe_xl::provision_replica', $primary_master_host,
+  run_task('pe_xl::provision_replica', $primary_master_host_local,
     primary_master_host    => $primary_master_host,
     primary_master_replica => $primary_master_replica_host,
   )
 
   run_task('pe_xl::pe_puppet_run', [
-    $primary_master_host,
+    $primary_master_host_local,
     $primary_master_replica_host,
   ])
 
-  run_task(pe_xl::configure_replica_db_node_group, $primary_master_host,
+  run_task(pe_xl::configure_replica_db_node_group, $primary_master_host_local,
     puppetdb_database_replica_host => $puppetdb_database_replica_host,
   )
   if $compile_master_hosts {
