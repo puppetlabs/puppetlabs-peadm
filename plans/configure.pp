8c9af142ae185ddb49552db449a6dd3174ac2a6a
Reid Vandewiele <reid@puppetlabs.com>
2018-09-11 16:34:21 -0700

    Clean up tasks and plans
    Remove task files not used
    
    Run Puppet one last time when all is done to finalize configuration

--- plans/configure.pp
+++ plans/configure.pp
@@ -1,6 +1,6 @@
 plan pe_xl::configure (
 
-  String[1]           $version = '2018.1.2',
+  String[1]           $version = '2018.1.3',
   String[1]           $console_password,
   Hash                $r10k_sources = { },
 
@@ -18,8 +18,8 @@ plan pe_xl::configure (
   String[1]           $stagingdir = '/tmp',
 ) {
 
-  # Retrieve and deploy a couple of modules from the Forge so that they can be
-  # used for ensuring some configuration.
+  # Retrieve and deploy the node_manager module from the Forge so that it can
+  # be used for ensuring some configuration.
   $nm_module_tarball = 'WhatsARanjit-node_manager-0.7.1.tar.gz'
   pe_xl::retrieve_and_upload(
     "https://forge.puppet.com/v3/files/${nm_module_tarball}",
@@ -28,16 +28,7 @@ plan pe_xl::configure (
     $primary_master_host
   )
 
-  $pexl_module_tarball = 'reidmv-pe_xl-master.tar.gz'
-  pe_xl::retrieve_and_upload(
-    'https://github.com/reidmv/reidmv-pe_xl/archive/master.tar.gz',
-    "${stagingdir}/${pexl_module_tarball}",
-    "/tmp/${pexl_module_tarball}",
-    $primary_master_host
-  )
-
   run_command("/opt/puppetlabs/bin/puppet module install /tmp/${nm_module_tarball}", $primary_master_host)
-  run_command("/opt/puppetlabs/bin/puppet module install /tmp/${pexl_module_tarball}", $primary_master_host)
   run_command('chown -R pe-puppet:pe-puppet /etc/puppetlabs/code', $primary_master_host)
 
   # Set up the console node groups to configure the various hosts in their
@@ -80,9 +71,12 @@ plan pe_xl::configure (
     primary_master_replica => $primary_master_replica_host,
   )
 
-  if $load_balancer_host {
-    run_task('pe_xl::puppet_runonce', $load_balancer_host)
-  }
+  # Run Puppet everywhere to pick up last remaining config tweaks
+  run_task('pe_xl::puppet_runonce', [
+    $primary_master_host,    $primary_master_replica_host,
+    $puppetdb_database_host, $puppetdb_database_replica_host,
+    $compile_master_hosts,   $load_balancer_host,
+  ].pe_xl::flatten_compact())
 
   return('Configuration of Puppet Enterprise with replica succeeded.')
 }
