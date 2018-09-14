d9c22ebfd73fa9b9c4638fd461b4838d9d27ca3b
Reid Vandewiele <reid@puppetlabs.com>
2018-09-14 14:48:44 -0700

    Rework upgrade not to file_upload PE
    Because PE is too big, basically. The orchestrator can't do it. Have the
    target nodes download it themselves instead from a source parameter.

--- plans/upgrade.pp
+++ plans/upgrade.pp
@@ -1,14 +1,15 @@
 plan pe_xl::upgrade (
-  String[1]           $primary_master_host,
-  String[1]           $puppetdb_database_host,
-  String[1]           $primary_master_replica_host,
-  String[1]           $puppetdb_database_replica_host,
-  Array[String[1]]    $compile_master_hosts = [ ],
+  String[1]        $primary_master_host,
+  String[1]        $puppetdb_database_host,
+  String[1]        $primary_master_replica_host,
+  String[1]        $puppetdb_database_replica_host,
+  Array[String[1]] $compile_master_hosts = [ ],
 
-#  String[1]           $console_password,
-  String[1]           $version = '2018.1.3',
+#  String[1]        $console_password,
+  String[1]        $version = '2018.1.3',
 
-  String[1]           $stagingdir = '/tmp',
+  String[1]        $stagingdir = '/tmp',
+  String[1]        $pe_source  = "https://s3.amazonaws.com/pe-builds/released/${version}/puppet-enterprise-${version}-el-7-x86_64.tar.gz",
 ) {
 
   $all_hosts = [
@@ -35,16 +36,16 @@ plan pe_xl::upgrade (
 
   # TODO: Do we need to update the pe.conf(s) with a console password?
 
-  # Download the PE tarball and send it to the nodes that need it
-  $pe_tarball_name     = "puppet-enterprise-${version}-el-7-x86_64.tar.gz"
-  $local_tarball_path  = "${stagingdir}/${pe_tarball_name}"
-  $upload_tarball_path = "/tmp/${pe_tarball_name}"
+  # Download the PE tarball on the nodes that need it
+  $upload_tarball_path = "/tmp/puppet-enterprise-${version}-el-7-x86_64.tar.gz"
 
-  pe_xl::retrieve_and_upload(
-    "https://s3.amazonaws.com/pe-builds/released/${version}/puppet-enterprise-${version}-el-7-x86_64.tar.gz",
-    $local_tarball_path,
-    $upload_tarball_path,
-    [$primary_master_host, $puppetdb_database_host, $puppetdb_database_replica_host],
+  run_task('pe_xl::download', [
+      $primary_master_host,
+      $puppetdb_database_host,
+      $puppetdb_database_replica_host
+    ],
+    source => $pe_source,
+    path   => $upload_tarball_path,
   )
 
   # Shut down Puppet on all infra hosts
