103ac5bc86b8f5140ac4a29235397018e41f0f7c
Reid Vandewiele <reid@puppetlabs.com>
2018-09-21 12:28:44 -0700

    Make puppet_runonce wait for in-progress runs
    So that if Puppet is already running when a runonce is requested, it
    will not fail a plan. Instead, the runonce will wait (up to 5 minutes)
    for the in-progress run to complete.

--- tasks/puppet_runonce.sh
+++ tasks/puppet_runonce.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-unset NOOP_FLAG
+[ "$PT_noop" = "true" ] && NOOP_FLAG="--noop" || unset NOOP_FLAG
 
-if [ "$PT_noop" = "true" ]; then
-  NOOP_FLAG="--noop"
-fi
+# Wait for up to five minutes for an in-progress Puppet agent run to complete
+# TODO: right now the check is just for lock file existence. Improve the check
+#       to account for situations where the lockfile is stale.
+echo -n "Check for and wait up to 5 minutes for in-progress run to complete: "
+lockfile=$(/opt/puppetlabs/bin/puppet config print agent_catalog_run_lockfile)
+n=0
+until [ $n -ge 300 ]
+do
+  [ ! -e "$lockfile" ] && break
+  echo -n .
+  n=$[$n+1]
+  sleep 1
+done
+echo
 
 /opt/puppetlabs/bin/puppet agent \
   --onetime \
