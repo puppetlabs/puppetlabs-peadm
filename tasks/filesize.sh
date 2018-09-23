20624975325dc36df4e2a39daf621d9804c88264
Reid Vandewiele <reid@puppetlabs.com>
2018-09-23 11:02:44 -0700

    Improve filesize task to not fail on absence
    If file is absent, filesize will now return size: nil, rather than
    failing. This is easier and simpler to deal with than catching task
    errors.

--- tasks/filesize.sh
+++ tasks/filesize.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-size=$(stat -c%s "$PT_path" 2>/dev/null)
+size=$(stat -c%s "$PT_path" 2>/dev/null || echo nil)
 
 # Output a JSON result for ease of Task usage in Puppet Task Plans
-echo '{ "size": "'$size'" }'
+if [ "$size" = "nil" ]; then
+  echo '{ "size": nil }'
+else
+  echo '{ "size": "'$size'" }'
+fi
