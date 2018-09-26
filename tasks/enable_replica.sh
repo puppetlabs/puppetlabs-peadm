21e04fac17b271c8d65a68d83c26e05b61625f18
Troy Klein <troy.klein@puppet.com>
2018-09-26 22:48:25 +0000

    added token file option to enable_replica

--- tasks/enable_replica.sh
+++ tasks/enable_replica.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+USER="${USER:=$(id -un)}"
+HOME="${HOME:=$(getent passwd "$USER" | cut -d : -f 6)}"
+
+if [[ "${PT_token_file}x" != 'x' ]] ; then
+  TOKEN_FILE="${PT_token_file}"
+else
+  TOKEN_FILE="${HOME}/.puppetlabs/token"
+fi
+
 set -e
 
 env PATH="/opt/puppetlabs/bin:${PATH}" \
-    USER="${USER:=$(id -un)}" \
-    HOME="${HOME:=$(getent passwd "$USER" | cut -d : -f 6)}" \
+    USER="${USER}" \
+    HOME="${HOME}" \
     puppet infrastructure enable replica "$PT_primary_master_replica" \
       --skip-agent-config \
       --topology mono-with-compile \
-      --yes
+      --yes --token-file "${TOKEN_FILE}"
