bb3dececb9f5d2f98f693d8eef4494f3b61607c0
Troy Klein <troy.klein@puppet.com>
2018-09-26 23:06:37 +0000

    changes to enable token_file to be a param

--- tasks/enable_replica.sh
+++ tasks/enable_replica.sh
@@ -4,7 +4,7 @@ USER="${USER:=$(id -un)}"
 HOME="${HOME:=$(getent passwd "$USER" | cut -d : -f 6)}"
 
 if [[ "${PT_token_file}x" != 'x' ]] ; then
-  TOKEN_FILE="${PT_token_file}"
+  TOKEN_FILE="$PT_token_file"
 else
   TOKEN_FILE="${HOME}/.puppetlabs/token"
 fi
@@ -12,9 +12,9 @@ fi
 set -e
 
 env PATH="/opt/puppetlabs/bin:${PATH}" \
-    USER="${USER}" \
-    HOME="${HOME}" \
+    USER="$USER" \
+    HOME="$HOME" \
     puppet infrastructure enable replica "$PT_primary_master_replica" \
       --skip-agent-config \
       --topology mono-with-compile \
-      --yes --token-file "${TOKEN_FILE}"
+      --yes --token-file "$TOKEN_FILE"
