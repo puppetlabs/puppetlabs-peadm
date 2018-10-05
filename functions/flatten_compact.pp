c3aed9af79f450d31c6e71a46f0ac6db394bfdc5
Reid Vandewiele <reid@puppetlabs.com>
2018-10-05 13:15:41 -0700

    Simplify pe_xl::flatten_compact code
    Use filter() instead of reduce()

--- functions/flatten_compact.pp
+++ functions/flatten_compact.pp
@@ -1,11 +1,7 @@
 function pe_xl::flatten_compact (
   Array $input,
 ) {
-  $input.flatten.reduce([]) |$output, $value| {
-    if ($value == undef) {
-      $output
-    } else {
-      $output << $value
-    }
+  $input.flatten.filter |$value| {
+    $value != undef
   }
 }
