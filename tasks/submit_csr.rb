da5493537192ba6265c1c123faefb0ad0b0779bd
Reid Vandewiele <reid@puppet.com>
2020-01-14 09:46:15 -0700

    Make CSR submission version-aware
    So that Puppet 5 (PE 2018.1) can be supported.

---
+++ tasks/submit_csr.rb
@@ -0,0 +1,40 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+#
+# rubocop:disable Style/GlobalVars
+require 'json'
+require 'open3'
+
+def main
+  params = JSON.parse(STDIN.read)
+  majver = %x{/opt/puppetlabs/bin/puppet --version}
+             .chomp
+             .split('.')
+             .first
+             .to_i
+
+  if majver < 6
+    conf = %x{puppet config print dns_alt_names certname}
+             .chomp
+             .split("\n")
+             .map {|line| line.split(' = ') }
+             .to_h
+
+    cmd = ['/opt/puppetlabs/bin/puppet', 'certificate', 'generate',
+           '--ca-location', 'remote',
+           '--dns-alt-names', conf['dns_alt_names'],
+           conf['certname']
+          ]
+  else
+    cmd = ['/opt/puppetlabs/bin/puppet', 'ssl', 'submit_request']
+  end
+
+  stdout, status = Open3.capture2(*cmd)
+  puts stdout
+  if status.success?
+    exit 0
+  else
+    exit 1
+  end
+end
+
+main
