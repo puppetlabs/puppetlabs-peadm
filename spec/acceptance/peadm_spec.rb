b201d99b4e3ca2e045b064ff97f5fff464c07061
Adam Buxton <buxton.adam@gmail.com>
2020-01-17 13:00:11 +0000

    erroneus placemnet corrected

---
+++ spec/acceptance/peadm_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper_acceptance'
+# @summary: default test does nothing
+def test_peadm()
+  
+    #return unless os[:family] != 'windows'
+    return unless os[:family] != 'Darwin'
+end
+
+describe 'default' do
+  context 'example acceptance do nothing' do
+    it do
+        test_peadm()
+    end
+  end
+end
