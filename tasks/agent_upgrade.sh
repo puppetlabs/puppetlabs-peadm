c2212f06147876cf92269912292251fa45289f29
Reid Vandewiele <reid@puppet.com>
2020-01-24 14:29:42 -0800

    Enforce PATH variable for agent_upgrade.sh
    Many of the tasks/plans shipped in PE are brittle and make assumptions
    about environment variables. By enforcing PATH (and HOME, and USER) in
    agent_upgrade.sh we hopefully insulate ourselves from the worst of it.

--- tasks/agent_upgrade.sh
+++ tasks/agent_upgrade.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export USER=$(id -un)
+export HOME=$(getent passwd "$USER" | cut -d : -f 6)
+export PATH="/opt/puppetlabs/bin:${PATH}"
+
 set -e
 
 curl -k "https://${PT_server}:8140/packages/current/upgrade.bash" | bash
