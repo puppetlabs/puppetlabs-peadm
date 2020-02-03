47222bf65097a1b86e7d6888bd3f7993fd49ac09
christopher.lawrence <chris.lawrence@puppet.com>
2020-02-03 19:21:13 +0000

    Adding agent_switching task and switch_master plan for changing Puppet Server and certificate regeneration

---
+++ plans/switch_master.pp
@@ -0,0 +1,47 @@
+plan peadm::switch_master (
+  TargetSpec $nodes,
+  String $master,
+  Boolean $sign_csr = false,
+
+){
+
+  # Extract the target node name and convert to certname array
+   $certname = get_targets($nodes).map |$a| { $a.name }
+
+  # Task to stop Puppet and change the Puppet Server name in puppet.conf
+    run_task ('peadm::agent_switchover',$nodes,
+      master => $master
+  )
+
+  # If the client does not have csr_attributes.yaml run the plan with sign_csr=true
+    if $sign_csr == true {
+
+    # Request a certificate from the master this command will fail so we set _catch_errors,
+    # this will allow the plan to run without exiting with an error
+      run_task('peadm::puppet_runonce', $nodes,
+        noop  =>  true,
+        _catch_errors => true
+      )  
+
+    # The Certificate is signed on the Puppet Server
+      run_task('peadm::sign_csr', $master,
+        certnames => $certname
+      )
+
+    }
+
+  # A certificate is requested and signed with autosign configurtiona and csr_attributes
+  # If there is an issue with the agent the plan will exit with an error
+    run_task('peadm::puppet_runonce', $nodes,
+      noop  =>  true,
+      _catch_errors => false
+    )
+
+  # Enable the Puppet Service 
+    run_task('service', $nodes,
+      action => 'start',
+      name   => 'puppet',
+    )  
+
+  return("Switching Puppet Master Certicate Regeneration succeeded.")
+}
---
+++ tasks/agent_switchover.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+export PATH=$PATH:/opt/puppetlabs/puppet/bin/
+
+# Puppet Task Name: 
+#
+# This is where you put the shell code for your task.
+#
+# You can write Puppet tasks in any language you want and it's easy to
+# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
+# https://puppet.com/docs/bolt/0.x/writing_tasks.html
+#
+# Puppet tasks make it easy for you to enable others to use your script. Tasks
+# describe what it does, explains parameters and which are required or optional,
+# as well as validates parameter type. For examples, if parameter "instances"
+# must be an integer and the optional "datacenter" parameter must be one of
+# portland, sydney, belfast or singapore then the .json file
+# would include:
+#   "parameters": {
+#     "instances": {
+#       "description": "Number of instances to create",
+#       "type": "Integer"
+#     },
+#     "datacenter": {
+#       "description": "Datacenter where instances will be created",
+#       "type": "Enum[portland, sydney, belfast, singapore]"
+#     }
+#   }
+# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
+#
+
+Date=$(date +"%m%d%Y")
+
+puppet resource service puppet ensure=stopped
+puppet resource service pxp-agent ensure=stopped
+mv `puppet config print ssldir` /tmp/ssl.${Date}
+puppet config set server $PT_master --section main
