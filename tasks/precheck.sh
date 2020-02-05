2f1451f7ac42a91fd6ffd3e6c517dd11628b628f
Reid Vandewiele <reid@puppet.com>
2020-02-05 13:10:41 -0800

    Fix precheck.sh script for RHEL
    In RHEL, VERSION_ID can be something like "7.3", rather than just "7".
    So for EL os's strip out the minor version when calculating the platform
    tag.

--- tasks/precheck.sh
+++ tasks/precheck.sh
@@ -5,8 +5,9 @@ osfamily=$(cat /etc/os-release | grep -qi ubuntu && echo "ubuntu" || echo "el")
 version=$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)
 arch=$(uname -m)
 
-# Because 64-bit Ubuntu wanted to be special.
+# OS-specific modifications
 [ "$osfamily" = "ubuntu" -a "$arch" = "x86_64" ] && arch="amd64"
+[ "$osfamily" = "el" ] && version=$(echo "$version" | cut -d . -f 1)
 
 # Output a JSON result for ease of Task usage in Puppet Task Plans
 cat <<EOS
