248bb5d8cd526f7ac70960d5c8103c741a8bfe7c
Bill Wilcox <bill.wilcox@puppet.com>
2020-02-20 22:56:51 -0800

    Address issue where group B wasn't created when a master replica was present without a standalone puppetdb replica.

--- manifests/setup/node_manager.pp
+++ manifests/setup/node_manager.pp
@@ -148,7 +148,9 @@ class peadm::setup::node_manager (
         },
       },
     }
+  }
 
+  if ($puppetdb_database_replica_host) {
     node_group { 'PE Compiler Group B':
       ensure  => 'present',
       parent  => 'PE Master',
@@ -159,6 +161,25 @@ class peadm::setup::node_manager (
       classes => {
         'puppet_enterprise::profile::puppetdb' => {
           'database_host' => $puppetdb_database_replica_host,
+        },
+        'puppet_enterprise::profile::master'   => {
+          'puppetdb_host' => ['${clientcert}', $master_host], # lint:ignore:single_quote_string_with_variables
+          'puppetdb_port' => [8081],
+        }
+      },
+      data    => $compiler_data,
+    }
+  } elsif ($master_replica_host) {
+    node_group { 'PE Compiler Group B':
+      ensure  => 'present',
+      parent  => 'PE Master',
+      rule    => ['and',
+        ['=', ['trusted', 'extensions', 'pp_application'], 'puppet/compiler'],
+        ['=', ['trusted', 'extensions', 'pp_cluster'], 'B'],
+      ],
+      classes => {
+        'puppet_enterprise::profile::puppetdb' => {
+          'database_host' => $puppetdb_database_host,
         },
         'puppet_enterprise::profile::master'   => {
           'puppetdb_host' => ['${clientcert}', $master_host], # lint:ignore:single_quote_string_with_variables
