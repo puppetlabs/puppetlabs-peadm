aaf660065e39ffa71909f5d853445e5cadebe86f
christopher.lawrence <chris.lawrence@puppet.com>
2020-03-11 10:52:27 +0000

    updated the agent_install.sh file sed statement for installation flags when installing an agent

--- tasks/agent_install.sh
+++ tasks/agent_install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-flags=$(echo $PT_install_flags | sed -e 's/^\["//' -e 's/\"]$//' -e 's/", *"/ /g')
+flags=$(echo $PT_install_flags | sed -e 's/^\["*//' -e 's/"*\]$//' -e 's/", *"/ /g')
 
 curl -k "https://${PT_server}:8140/packages/current/install.bash" | bash -s -- $flags
