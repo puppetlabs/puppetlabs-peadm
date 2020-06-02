440244af6a1b206aad96367359402d85cfa9fcfd
Reid Vandewiele <reid@puppet.com>
2020-06-02 13:10:03 -0700

    Add convert compatibility for PE 2018.1
    Puppet 5, doesn't have the `puppet ssl` command.

--- plans/util/add_cert_extensions.pp
+++ plans/util/add_cert_extensions.pp
@@ -39,6 +39,7 @@ plan peadm::util::add_cert_extensions (
   # Loop through and recert each target one at at time, because Bolt lacks
   # real parallelism
   $all_targets.map |$target| {
+    $certname = $certdata[$target]['certname']
 
     # This will be the new trusted fact data for this node
     $extension_requests = $certdata[$target]['extensions'] + $extensions
@@ -50,24 +51,28 @@ plan peadm::util::add_cert_extensions (
     )
 
     # Everything starts the same; we always revoke the existing cert
-    run_command("${pserver} ca clean --certname ${certdata[$target]['certname']}", $master_target)
+    run_command("${pserver} ca clean --certname ${certname}", $master_target)
 
     # Then things get crazy...
 
-    # The procedure for regenerating an agent's cert
     if ($certdata[$target]['certname'] != $master_certname) {
-      run_command("${puppet} ssl clean --certname ${certdata[$target]['certname']}", $target)
-      run_command("${puppet} ssl submit_request --certname ${certdata[$target]['certname']}", $target)
+      # AGENT cert regeneration
+      run_task('peadm::ssl_clean', $target, certname => $certname)
+      run_task('peadm::submit_csr', $target)
       ctrl::sleep(2) # some lag sometimes before the cert is available to sign
-      run_command(@("HEREDOC"/L), $master_target)
-        ${pserver} ca sign --certname ${certdata[$target]['certname']} || \
-        ${pserver} ca list --certname ${certdata[$target]['certname']} \
-        | HEREDOC
-      run_command("${puppet} ssl download_cert --certname ${certdata[$target]['certname']}", $target)
-    }
+      run_task('peadm::sign_csr', $master_target, certnames => [$certname])
 
-    # The procedure for regenerating the master's cert
+      # Use a command instead of a task so that this works for Puppet 5 agents
+      # w/ PCP transport. If using a task, we run into problems downloading
+      # the task file at this point, because there is no longer a cert file
+      # present on the agent.
+      run_command(@("HEREDOC"/L), $target)
+        ${puppet} ssl download_cert --certname ${certname} || \
+        ${puppet} certificate find --ca-location remote ${certname}
+        | HEREDOC
+    }
     else {
+      # MASTER cert regeneration
       # Store the node's current dns-alt-names, for use as a flag restoring
       # them later
       $alt_names_flag = $certdata[$target]['dns-alt-names'] ? {
