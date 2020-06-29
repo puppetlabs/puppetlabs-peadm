a1593b407718fe57a9d7248752f46be5be1e7724
Reid Vandewiele <reid@puppet.com>
2020-06-29 12:22:48 -0700

    Fix function call error
    Missing path argument

--- plans/util/sanitize_pg_pe_conf.pp
+++ plans/util/sanitize_pg_pe_conf.pp
@@ -4,16 +4,17 @@ plan peadm::util::sanitize_pg_pe_conf (
 ) {
   $master_target = get_target($master_host)
 
+  $path = '/etc/puppetlabs/enterprise/conf.d/pe.conf'
   # Ensure the pe.conf file on PostgreSQL nodes has the needed values for
   # puppet_master_host and database_host
   run_task('peadm::read_file', $targets,
-    path => '/etc/puppetlabs/enterprise/conf.d/pe.conf',
+    path => $path,
   ).map |$result| {
     $sanitized = $result['content'].loadjson() + {
       'puppet_enterprise::puppet_master_host' => $master_target.peadm::target_name(),
       'puppet_enterprise::database_host'      => $result.target.peadm::target_name(),
     }
     # Return the result of file_content_upload. There is only one target
-    peadm::file_content_upload($sanitized, $result.target)[0]
+    peadm::file_content_upload($sanitized, $path, $result.target)[0]
   }
 }
