f647a34804255435ba17474783adc070f4c3c7c9
kreeuwijk <kevin.reeuwijk@puppet.com>
2021-03-10 14:40:06 +0100

    initial script

--- tasks/migrate_agents_win.ps1
+++ tasks/migrate_agents_win.ps1
@@ -1,1 +1,62 @@
-# test
+[CmdletBinding()]
+Param(
+  [String] $target_pe,
+  [Boolean] $regenerate = $true
+)
+
+# Ensure we can reach the target Puppet Enterprise server
+Write-Host "Verifying connectivity to target Puppet server $target_pe:8140..."
+if (Test-NetConnection -ComputerName $target_pe -Port 8140) {
+    Write-Host "Target Puppet server is reacheable"
+} else {
+    Write-Host "Target Puppet server is not reacheable, aborting migration!"
+    exit 1
+}
+
+Write-Host 
+Write-Host "Stopping the Puppet Agent service..."
+Stop-Service puppet
+
+if ($regenerate) {
+    Write-Host 
+    Write-Host "Regenerate flag detected, clearing out existing node certificates before migration..."
+    $localcacert = puppet config print localcacert
+    $hostcert    = puppet config print hostcert
+    $hostprivkey = puppet config print hostprivkey
+    $hostpubkey  = puppet config print hostpubkey
+    $hostcrl     = puppet config print hostcrl
+    $hostcsr     = puppet config print hostcsr
+    $collection = $localcacert, $hostcert, $hostprivkey, $hostpubkey, $hostcrl, $hostcsr
+    foreach ($item in $collection) {
+        if (Test-Path $item -PathType leaf) {
+            Write-Host "Deleting $item..."
+            Remove-Item $item
+        } 
+    }
+    Write-Host "Existing node certificates have been cleared, new certificates will be generated on the next Puppet run"
+}
+
+Write-Host 
+Write-Host "Pointing Puppet Agent to new PE server..."
+puppet config set server $target_pe
+
+Write-Host 
+Write-Host "Installing the new Puppet Agent..."
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile("https://$target_pe:8140/packages/current/install.ps1", "$env:TEMP\install.ps1")
+& $env:TEMP\install.ps1 -v 2>&1
+
+if ($? -gt 0) {
+    Write-Host "Installation of new Puppet Agent failed! Check the error for details."
+    exit 3
+}
+
+Write-Host 
+Write-Host "Performing initial Puppet Agent run..."
+puppet agent --no-daemonize --onetime --no-usecacheonfailure --no-splay  2>&1
+
+Write-Host 
+Write-Host "Starting the Puppet Agent service..."
+Start-Service puppet
