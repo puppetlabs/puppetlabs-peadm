faeeaf745d2f916fe5f0c9477ca32951abf292b8
Cody Herriges <193064+ody@users.noreply.github.com>
2021-06-09 22:09:18 +0000

    Support lates PE releases
    Sets the upper bound for supported PE to something that matches a
    released version of 2021.

--- functions/assert_supported_pe_version.pp
+++ functions/assert_supported_pe_version.pp
@@ -3,16 +3,18 @@
 function peadm::assert_supported_pe_version (
   String $version,
 ) >> Struct[{'supported' => Boolean}] {
-  $supported = ($version =~ SemVerRange('>= 2019.7.0 <= 2021.0.0'))
+  $oldest = '2019.7'
+  $newest = '2021.2'
+  $supported = ($version =~ SemVerRange(">= ${oldest} <= ${newest}"))
 
   unless $supported {
     fail(@("REASON"/L))
       This version of the puppetlabs-peadm module does not support PE ${version}.
 
-      For PE versions older than 2019.7, please use version 1.x of the \
+      For PE versions older than ${oldest}, please use version 1.x of the \
       puppetlabs-peadm module.
 
-      For PE versions newer than 2021.0, check to see if a new version of peadm \
+      For PE versions newer than ${newest}, check to see if a new version of peadm \
       exists which supports that version of PE.
 
       | REASON
