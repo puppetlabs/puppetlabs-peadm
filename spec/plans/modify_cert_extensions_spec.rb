20de8c93e6d6036591b1142ad353512ebfba9fe2
Reid Vandewiele <reid@puppet.com>
2021-06-11 16:30:01 -0700

    Update tests for convert, modify_cert_extensions

---
+++ spec/plans/modify_cert_extensions_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'peadm::modify_cert_extensions' do
+  include BoltSpec::Plans
+
+  describe 'basic functionality' do
+    context 'modifying multiple client certificates' do
+      let(:params) do
+        {
+          'targets'      => 'foo,primary,bar',
+          'primary_host' => 'primary',
+          'add'          => { 'pe_role' => 'puppet/server' },
+        }
+      end
+
+      it 'runs successfully ' do
+        pending('a BoltSpec bug related to parallelize() being fixed')
+
+        allow_task('peadm::cert_data').always_return({ 'certname' => 'primary' })
+        expect_plan('peadm::subplans::modify_cert_extensions').be_called_times(3)
+
+        expect(run_plan('peadm::modify_cert_extensions', params)).to be_ok
+      end
+    end
+  end
+end
