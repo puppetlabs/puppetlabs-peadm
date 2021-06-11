139a33d11678625fd4274fb14aee4b2ff46e56e0
David Sandilands <david@puppet.com>
2021-06-11 11:12:02 +0100

    (Solarch-278) still an issue with function file_content_upload but almost there

---
+++ spec/plans/action/install_spec.rb
@@ -0,0 +1,35 @@
+# spec/spec_helper.rb
+
+# Load the BoltSpec library
+require 'bolt_spec/plans'
+
+describe 'peadm::action::install' do
+  # Include the BoltSpec library functions
+  include BoltSpec::Plans
+
+  # Configure Puppet and Bolt before running any tests
+  before(:all) do
+    BoltSpec::Plans.init
+  end
+
+
+  it 'minimum variables to run' do
+    allow_task('peadm::precheck').return_for_targets(
+      'primary' => {
+        'hostname' => 'primary',
+        'platform' => 'el-7.11-x86_64'
+      }
+    )
+    expect_task('peadm::mkdir_p_file').be_called_times(4)
+    expect_plan('peadm::util::retrieve_and_upload')
+    expect_task('peadm::read_file')
+    expect_task('peadm::pe_install')
+    expect_command('systemctl stop pe-puppetdb')
+    expect_command('systemctl start pe-puppetdb')
+    expect_task('peadm::rbac_token')
+    expect_task('peadm::code_manager')
+    expect_task('peadm::puppet_runonce').be_called_times(2)
+    expect_task('peadm::wait_until_service_ready')
+    expect(run_plan('peadm::action::install', 'primary_host' => 'primary', 'console_password' => 'puppetlabs')).to be_ok
+ end
+end
