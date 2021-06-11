6703a44e22dbf5cfaf239b93a27f7d214d58df9d
David Sandilands <david@puppet.com>
2021-06-11 17:00:34 +0100

    (SOLARCH-278) uploading this to review why even with bolt result still returns file not found

--- spec/plans/util/retrieve_and_upload_spec.rb
+++ spec/plans/util/retrieve_and_upload_spec.rb
@@ -12,35 +12,21 @@ describe 'peadm::util::retrieve_and_upload' do
     BoltSpec::Plans.init
   end
 
-  let(:source) { 'http://downloadsite.com/file' }
-  let(:local_path) { '/tmp/download' }
-  let(:upload_path) { '/tmp/upload' }
-  let(:nodes) { 'ssh://root:letmein@localhost:22' }
-  let(:targets) { 'localhost' }
-
-  #it 'file is downloaded and uploaded' do
-  #  expect_command("test -e '/tmp/download'").always_return({'stdout' => 'im here'})
-  #  expect_task('peadm::download').not_be_called
-  #  expect_task('peadm::filesize').always_return('size' => '1').be_called_times(2)
-  #  allow_upload('/tmp/download')
-  #  expect(run_plan('peadm::util::retrieve_and_upload', 'nodes' => 'host', 'source' => 'http://downloadsite.com/file', 'upload_path' => '/tmp/upload', 'local_path' => '/tmp/download')).to be_ok
-  #end
-
-  #it 'file needs downloaded and needs uploaded' do
-  #  expect_command("test -e '/tmp/download'").error_with('kind' => 'nope', 'msg' => 'The command failed with exit code 1')
-  #  expect_task('peadm::download')
-  #  expect_task('peadm::filesize').be_called_times(2).return_for_targets(
-  #    'local://localhost' => { 'size' => '2'},
-  #    'localhost' => { 'size' => 'null'}
-  #  )
-  #  expect_upload('/tmp/download').return do |targets, source, upload_path |
-  #    results = targets.map do |target|
-  #    Bolt::Result.new(target, value: { 'path' => File.join(upload_path, source) })
-  #    end
-  #  Bolt::ResultSet.new(results)
-  #  end
+  it 'file needs downloaded and needs uploaded' do
+    expect_command("test -e '/tmp/download'").error_with('kind' => 'nope', 'msg' => 'The command failed with exit code 1')
+    expect_task('peadm::download')
+    expect_task('peadm::filesize').be_called_times(2).return_for_targets(
+      'local://localhost' => { 'size' => '2'},
+      'primary' => { 'size' => 'null'}
+    )
+    expect_upload('/tmp/download').with_destination('/tmp/upload').with_params({}).return do |targets, source, destination, params|
+      results = targets.map do |target|
+        Bolt::Result.new(target, value: { 'path' => File.join(destination, source) })
+      end
     
-  #  expect(run_plan('peadm::util::retrieve_and_upload', 'nodes' => nodes, 'source' => source, 'upload_path' => upload_path, 'local_path' => local_path)).to be_ok
-  #end
-
+      Bolt::ResultSet.new(results)
+    end
+    
+   expect(run_plan('peadm::util::retrieve_and_upload', 'nodes' => 'primary', 'source' => '/tmp/source', 'upload_path' => '/tmp/upload', 'local_path' => '/tmp/download')).to be_ok
+  end
 end
