ee634c9ae5644ae2a600bcce32c665058436e1ef
Reid Vandewiele <reid@puppet.com>
2021-06-14 15:47:55 -0700

    pdk validate lint fixes

--- spec/plans/action/install_spec.rb
+++ spec/plans/action/install_spec.rb
@@ -7,11 +7,6 @@ describe 'peadm::action::install' do
   # Include the BoltSpec library functions
   include BoltSpec::Plans
 
-  # Configure Puppet and Bolt before running any tests
-  before(:all) do
-    BoltSpec::Plans.init
-  end
-
   # something needs done here for the function
 
   it 'minimum variables to run' do
@@ -19,7 +14,7 @@ describe 'peadm::action::install' do
       'primary' => {
         'hostname' => 'primary',
         'platform' => 'el-7.11-x86_64'
-      }
+      },
     )
     expect_task('peadm::mkdir_p_file').be_called_times(4)
     expect_plan('peadm::util::retrieve_and_upload')
@@ -31,10 +26,10 @@ describe 'peadm::action::install' do
     expect_task('peadm::code_manager')
     expect_task('peadm::puppet_runonce').be_called_times(2)
     expect_task('peadm::wait_until_service_ready')
-  #  expect(run_plan('peadm::action::install', 'primary_host' => 'primary', 'console_password' => 'puppetlabs')).to be_ok
-#### Currently functions are not mockable in bolt testing as per 
-#### https://github.com/puppetlabs/bolt/issues/1812 to work around us use
-#### above testing you can comment out function peadm::file_content_upload
-#### in the puppet code and uncomment the expect above to test and confirm
-end
+    #  expect(run_plan('peadm::action::install', 'primary_host' => 'primary', 'console_password' => 'puppetlabs')).to be_ok
+    #### Currently functions are not mockable in bolt testing as per
+    #### https://github.com/puppetlabs/bolt/issues/1812 to work around us use
+    #### above testing you can comment out function peadm::file_content_upload
+    #### in the puppet code and uncomment the expect above to test and confirm
+  end
 end
--- spec/plans/util/retrieve_and_upload_spec.rb
+++ spec/plans/util/retrieve_and_upload_spec.rb
@@ -7,14 +7,14 @@ describe 'peadm::util::retrieve_and_upload' do
     expect_command("test -e '/tmp/download'").error_with('kind' => 'nope', 'msg' => 'The command failed with exit code 1')
     expect_task('peadm::download')
     expect_task('peadm::filesize').be_called_times(2).return_for_targets(
-      'local://localhost' => { 'size' => '2'},
-      'primary' => { 'size' => 'null'}
+      'local://localhost' => { 'size' => '2' },
+      'primary' => { 'size' => 'null' },
     )
-    expect_upload('/tmp/download').with_destination('/tmp/upload').with_params({}).return do |targets, source, destination, params|
+    expect_upload('/tmp/download').with_destination('/tmp/upload').with_params({}).return do |targets, source, destination, _params|
       results = targets.map do |target|
         Bolt::Result.new(target, value: { 'path' => File.join(destination, source) })
       end
-    
+
       Bolt::ResultSet.new(results)
     end
 
