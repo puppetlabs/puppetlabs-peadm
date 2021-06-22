fb0a9347b9517b4519b66281e7c763ecd495543a
Dimitri Tischenko <dimitri@puppet.com>
2021-06-22 13:02:33 +0200

    Create peadm_spec::add_replica test plan
    For the pending peadm::add_replica plan

---
+++ spec/fixtures/modules/peadm_spec/plans/add_replica.pp
@@ -0,0 +1,26 @@
+plan peadm_spec::add_replica(
+){
+
+  $t = get_targets('*')
+  wait_until_available($t)
+
+  parallelize($t) |$target| {
+    $fqdn = run_command('hostname -f', $target)
+    $target.set_var('certname', $fqdn.first['stdout'].chomp)
+  }
+
+  $primary_host = $t.filter |$n| { $n.vars['role'] == 'primary' }
+  $replica_host = $t.filter |$n| { $n.vars['role'] == 'replica' }
+  $replica_postgresql_host = $t.filter |$n| { $n.vars['role'] == 'replica-pdb-postgresql' }
+
+  if $replica_host == [] {
+    fail_plan('"replica" role missing from inventory, cannot continue')
+  }
+
+  run_plan('peadm::add_replica',
+    primary_host            => $primary_host,
+    replica_host            => $replica_host,
+    replica_postgresql_host => $replica_postgresql_host ? { [] => undef, default => $replica_postgresql_host },
+  )
+
+}
