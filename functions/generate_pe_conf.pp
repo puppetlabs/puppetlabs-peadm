065e8e11837ee0aa0212a8f8a9afcf25175b5d68
Reid Vandewiele <reid@puppet.com>
2021-07-26 10:33:17 -0700

    Remove defaults code in generate_pe_conf
    Removing for now as there is no need to set peadm-coded default pe.conf
    configuration values.
    
    To restore the ability to inject defaults into generated pe.conf files,
    revert this commit.

--- functions/generate_pe_conf.pp
+++ functions/generate_pe_conf.pp
@@ -12,35 +12,9 @@ function peadm::generate_pe_conf (
     fail('pe.conf must have the console_admin_password set')
   }
 
-  # Define the configuration settings that will be placed in pe.conf by
-  # default. These can be overriden by user-supplied values in the $settings
-  # hash.
-  #
-  # At this time, there are no defaults which need to be modified from the
-  # out-of-box defaults.
-  $defaults = {
-    # 'puppet_enterprise::profile::master::java_args' => {
-    #   'Xmx' => '2048m',
-    #   'Xms' => '512m',
-    # },
-    # 'puppet_enterprise::profile::console::java_args' => {
-    #   'Xmx' => '768m',
-    #   'Xms' => '256m',
-    # },
-    # 'puppet_enterprise::profile::orchestrator::java_args' => {
-    #   'Xmx' => '768m',
-    #   'Xms' => '256m',
-    # },
-    # 'puppet_enterprise::profile::puppetdb::java_args' => {
-    #   'Xmx' => '768m',
-    #   'Xms' => '256m',
-    # },
-  }
-
-  # Merge the defaults with user-supplied settings, remove anything that is
-  # undef, then output to JSON (and therefore HOCON, because HOCON is a
-  # superset of JSON)
-  ($defaults + $settings).filter |$key,$value| {
+  # Remove anything that is undef, then output to JSON (and therefore HOCON,
+  # because HOCON is a superset of JSON)
+  $settings.filter |$key,$value| {
     $value != undef
   }.to_json_pretty()
 }
