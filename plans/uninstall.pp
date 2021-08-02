b384b06e3316cfc1042be598c710253214a283fb
Ed Mckain <mckain.default@gmail.com>
2021-08-02 13:49:36 -0600

    removes the  param from uninstall

--- plans/uninstall.pp
+++ plans/uninstall.pp
@@ -1,13 +1,9 @@
 # @summary Single-entry-point plan for uninstalling Puppet Enterprise
 
 plan peadm::uninstall (
-  # Standard
-  Peadm::SingleTargetSpec           $primary_host,
-  # Common Configuration
-  String                            $version       = '2019.8.5',
+  Peadm::SingleTargetSpec  $primary_host,
 ) {
   peadm::assert_supported_bolt_version()
-  peadm::assert_supported_pe_version($version)
 
   $primary_target = peadm::get_targets($primary_host, 1)
   $uninstall_result = run_task('peadm::pe_uninstall', $primary_target)
