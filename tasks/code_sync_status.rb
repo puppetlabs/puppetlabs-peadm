3a6129e340979d57beab801a66789bfdff758492
David Sandilands <david@puppet.com>
2021-08-11 17:55:51 +0100

    (SOLARCH-558) still work in progress needing to get ruby params and structure right but basics are there

--- tasks/code_sync_status.rb
+++ tasks/code_sync_status.rb
@@ -4,11 +4,14 @@
 require 'net/https'
 require 'uri'
 require 'json'
-require 'fileutils'
+require 'logger'
 
 # Parameters expected:
 #   Hash
 #     Array requestedenvironments
+$params = JSON.parse(STDIN.read)
+#$logger = Logger.new(STDOUT)
+#$logger.level = ($params['debug']) ? Logger::DEBUG : Logger::INFO
 
 uri = URI.parse('https://localhost:8140/status/v1/services?level=debug')
 http = Net::HTTP.new(uri.host, uri.port)
@@ -16,16 +19,43 @@ http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 response = http.request(Net::HTTP::Get.new(uri.request_uri))
 raise "API failure https://localhost:8140/status/v1/services returns #{response.code}" unless response.is_a? Net::HTTPSuccess
-#enivronments = reponse.body['filesync-storage-service']['status']['clients'][server]['repos']['puppet-code']['submodules'].keys()
 servers = JSON.parse(response.body)['file-sync-storage-service']['status']['clients'].keys
 environments  = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules'].keys()
-for server in servers do
-  puts server
+
+if params['requestedenvironments'].any?{ |s| s.casecmp("all")==0 }
+  puts "Checking all deployed environments"
+else
+  for environment in params['requestedenvironments']
+    if environments.any?{ |s| s.casecmp("#{environment}")==0 }
+      puts "Environment #{environment} is visible and will be checked"
+# add to some sort of list
+    else
+      puts "Environment #{environment} is not visible and will not be checked"
+    end
+  end
+end
+
+#if environments contains all ALL or any other combo crack on with the lot
+# else
+# check our passed in environments do they exist
+# if they dont output they dont and remove from list
+for environment in environments
+  puts environment
   puts '---'
-  for environment in environments
-    puts environment
-    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients']["#{server}"]['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message']
+  match = true
+  for server in servers do
+    puts server
+    puts '---'
+    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients']["#{server}"]['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
+    primarycommit = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
+# Error check here that it is not the default no code manager message and it at least vageuly looks like a sha1 /\b([a-f0-9]{40})\b/
     puts servercommit
+    puts primarycommit
+    if servercommit == primarycommit
+      puts "#{server} in sync for #{environment}"
+    else
+      puts "#{server} out of sync for #{environment}"
+    end
   end
 end
 
