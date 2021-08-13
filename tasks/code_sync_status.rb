e180955210a04e56254d82b22a28a1ae7f28e43e
David Sandilands <david@puppet.com>
2021-08-13 16:16:24 +0100

    (SOLARCH-558) correcting linting errors

--- tasks/code_sync_status.rb
+++ tasks/code_sync_status.rb
@@ -21,40 +21,37 @@ raise "API failure https://localhost:8140/status/v1/services returns #{response.
 # Get list of servers from filesync service
 servers = JSON.parse(response.body)['file-sync-storage-service']['status']['clients'].keys
 # Get list of environments from filesync service
-environments  = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules'].keys()
-environmentstocheck = Array.new
+environments = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules'].keys
+environmentstocheck = []
 
 # If all was passed as an argument we check all visible environments
-if params['environments'].any?{ |s| s.casecmp("all")==0 }
+if params['environments'].any? { |s| s.casecmp('all') == 0 }
   environmentstocheck = environments
 # Else check each requested environment to confirm its a visible environment
 else
-  for environment in params['environments']
-    if environments.any?{ |s| s.casecmp("#{environment}")==0 }
-      environmentstocheck << environment
-    else
-      raise "Environment #{environment} is not visible and will not be checked"
-    end
+  params['environments'].each do |environment|
+    environments.any? { |s| s.casecmp(environment.to_s) == 0 } || raise("Environment #{environment} is not visible and will not be checked")
+    environmentstocheck << environment
   end
 end
 results = {}
 # Run status of the script assume its good until it we hit a failure
 scriptstatus = true
-for environment in environmentstocheck
+environmentstocheck.each do |environment|
   results[environment] = {}
   # The status of this environment assume its good until we hit a failure
   environmentmatch = true
   # Find the commit ID of the environment accroding to the file sync service
-  primarycommit = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
+  primarycommit = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules'][environment.to_s]['latest_commit']['message'][32..71]
   results[environment]['latest_commit'] = primarycommit
-  for server in servers do
+  servers.each do |server|
     results[environment][server] = {}
-    # Find the commit ID of the server we are checking for this environment 
-    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients']["#{server}"]['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]   
-    results [environment][server]['commit'] = servercommit  
+    # Find the commit ID of the server we are checking for this environment
+    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients'][server.to_s]['repos']['puppet-code']['submodules'][environment.to_s]['latest_commit']['message'][32..71]
+    results [environment][server]['commit'] = servercommit
     # Check if it matches and if not mark the environment and script as having a server not in sync on an environment
     if servercommit == primarycommit
-      results [environment][server]['sync'] = true  
+      results [environment][server]['sync'] = true
     else
       results [environment][server]['sync'] = false
       environmentmatch = false
@@ -62,19 +59,19 @@ for environment in environmentstocheck
     end
   end
   # write to the result json if its a match for the environment
-  if environmentmatch
-    results [environment]['in_sync'] = true
-  else
-    results [environment]['in_sync'] = false
-  end
+  results [environment]['in_sync'] = if environmentmatch
+                                       true
+                                     else
+                                       false
+                                     end
 end
 # Write to the result json if for all environments checked if its a match
 if scriptstatus
-    results['in_sync'] = true
-    puts results.to_json
-    exit 0
-else    
-    results['in_sync'] = false
-    puts results.to_json
-    exit 1
+  results['in_sync'] = true
+  puts results.to_json
+  exit 0
+else
+  results['in_sync'] = false
+  puts results.to_json
+  exit 1
 end
