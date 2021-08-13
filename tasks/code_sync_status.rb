1ceb66e71d60fb5bb9d41c94b81f1fed11bbfe14
David Sandilands <david@puppet.com>
2021-08-13 14:20:04 +0100

    (SOLARCH-558) changing to logging and returning json output

--- tasks/code_sync_status.rb
+++ tasks/code_sync_status.rb
@@ -10,8 +10,8 @@ require 'logger'
 #   Hash
 #     Array requestedenvironments
 params = JSON.parse(STDIN.read)
-#$logger = Logger.new(STDOUT)
-#$logger.level = ($params['debug']) ? Logger::DEBUG : Logger::INFO
+$logger = Logger.new(STDOUT)
+$logger.level = ($params['debug']) ? Logger::DEBUG : Logger::INFO
 
 # Only debug level includes code sync details
 uri = URI.parse('https://localhost:8140/status/v1/services?level=debug')
@@ -29,54 +29,64 @@ environmentstocheck = Array.new
 
 # If all was passed as an argument we check all visible environments
 if params['environments'].any?{ |s| s.casecmp("all")==0 }
-  puts "Checking all deployed environments"
+  logger.debug("Checking all deployed environments")
   environmentstocheck = environments
 # Else check each requested environment to confirm its a visible environment
 else
   for environment in params['environments']
     if environments.any?{ |s| s.casecmp("#{environment}")==0 }
-      puts "Environment #{environment} is visible and will be checked"
+        logger.debug("Environment #{environment} is visible and will be checked")
       environmentstocheck << environment
     else
-      puts "Environment #{environment} is not visible and will not be checked"
+        logger.error("Environment #{environment} is not visible and will not be checked")
     end
   end
 end
-
+results {}
 # Run status of the script assume its good until it we hit a failure
 scriptstatus = true
 for environment in environmentstocheck
-  puts environment
-  puts '---'
+  results[environment] = {}
 # The status of this environment assume its good until we hit a failure
   environmentmatch = true
+  # Find the commit ID of the environment accroding to the file sync service
+  primarycommit = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
+  results[environment]['latest_commit']
   for server in servers do
-    puts server
-    puts '---'
-# Find the commit ID of the server we are checking for this environment
-    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients']["#{server}"]['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
-# Find the commit ID of the environment accroding to the file sync service
-    primarycommit = JSON.parse(response.body)['file-sync-storage-service']['status']['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]
-# Error check here that it is not the default no code manager message and it at least vageuly looks like a sha1 /\b([a-f0-9]{40})\b/
-    puts servercommit
-    puts primarycommit
-# Check if it matches and if not mark the environment and script as having a server not in sync on an environment
+    logger.debug("Checking #{server}")
+    results[environment][server] = {}
+    # Find the commit ID of the server we are checking for this environment 
+    servercommit = JSON.parse(response.body)['file-sync-storage-service']['status']['clients']["#{server}"]['repos']['puppet-code']['submodules']["#{environment}"]['latest_commit']['message'][32..71]   
+    # Error check here that it is not the default no code manager message and it at least vageuly looks like a sha1 /\b([a-f0-9]{40})\b/
+    results [environment][server][servercommit] = {}   
+    # Check if it matches and if not mark the environment and script as having a server not in sync on an environment
     if servercommit == primarycommit
-      puts "#{server} in sync for #{environment}"
+      logger.debug("#{server} in sync for #{environment}")
+      results [environment][server]['sync'] = true  
     else
-      puts "#{server} out of sync for #{environment}"
+      logger.error("#{server} out of sync for #{environment}")
+      results [environment][server]['sync'] = false
       environmentmatch = false
       scriptstatus = false
     end
   end
   if environmentmatch
-    puts "#{environment} in sync"
+    logger.debug("#{environment} in sync")
+    results [environment]['sync'] = true
   else
-    puts "#{environment} out of sync"
+    logger.error("#{environment} out of sync")
+    results [environment]['sync'] = false
   end
 end
 if scriptstatus
-  puts "All environments in sync"
+    logger.debug("All environments in sync")
+    results[sync] = true
+    puts results.to_json
+    exit 0
 else
-  puts "Not all environments were in sync"
+    
+    results[sync] = false
+    puts results.to_json
+    logger.error("Not all environments were in sync")
+    exit 1
 end
