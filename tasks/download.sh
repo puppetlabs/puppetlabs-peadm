5d9de2444399814d125e7de313d93c174b0a2dde
Dimitri Tischenko <dimitri@puppet.com>
2021-08-31 15:57:02 +0200

    Revert "add -x bash option for debugging"
    This reverts commit fa6117611820fdb29f4a626662536248bd67cdb5.

--- tasks/download.sh
+++ tasks/download.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 urisize=$(curl -s -L --head "$PT_source" | sed -n 's/Content-Length: \([0-9]\+\)/\1/p' | tr -d '\012\015')
 filesize=$(stat -c%s "$PT_path" 2>/dev/null)
