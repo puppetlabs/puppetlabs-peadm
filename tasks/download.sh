d1ae598feddaf5c23ac8ea1569f10567e8a6c31e
Edwin Maldonado <mckain.default@gmail.com>
2021-09-27 17:57:49 +0200

    adds spaces around the brackets to meet bash syntax
    Co-authored-by: Reid Vandewiele <reid@puppet.com>

--- tasks/download.sh
+++ tasks/download.sh
@@ -41,7 +41,7 @@ download() {
   tmp_file=$(mktemp -p /tmp/ "$tmp_file_name")
   echo "Temporary file created at: ${tmp_file}"
   curl -s -f -L -o ${tmp_file} "$1"
-  if [tar -tzf ${tmp_file} >/dev/null]; then
+  if [ tar -tzf "${tmp_file}" >/dev/null ]; then
     mv ${tmp_file} "$2"
   else
     echo "Puppet Enterprise download failed: Invalid tarball"
