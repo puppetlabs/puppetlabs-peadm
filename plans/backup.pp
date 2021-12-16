845842ff3f0d0e077e30ada0bb9af0906e383f06
David Sandilands <david@puppet.com>
2021-12-16 13:51:18 +0000

    SOLARCH-564 test running simply classification and cert backup

--- plans/backup.pp
+++ plans/backup.pp
@@ -3,14 +3,25 @@
 # This plan can backup data as outlined at insert doc 
 # 
 plan peadm::backup (
-  TargetSpec              $targets,
-  Boolean                 $backup_orchestrator = true,
-  Boolean                 $backup_rbac = true,
-  Boolean                 $backup_activity = true,
-  Boolean                 $backup_ca_ssl = true,
-  Boolean                 $backup_puppetdb = false,
-  Boolean                 $backup_classification = true,
-  String                  $backup_directory =/tmp/
+  # Standard
+  Peadm::SingleTargetSpec           $primary_host,
+  Optional[Peadm::SingleTargetSpec] $replica_host            = undef,
+
+  # Large
+  Optional[TargetSpec]              $compiler_hosts          = undef,
+
+  # Extra Large
+  Optional[Peadm::SingleTargetSpec] $primary_postgresql_host = undef,
+  Optional[Peadm::SingleTargetSpec] $replica_postgresql_host = undef,
+
+  # Which data to backup
+  Boolean                            $backup_orchestrator    = true,
+  Boolean                            $backup_rbac            = true,
+  Boolean                            $backup_activity        = true,
+  Boolean                            $backup_ca_ssl          = true,
+  Boolean                            $backup_puppetdb        = false,
+  Boolean                            $backup_classification  = true,
+  String                             $backup_directory       = '/tmp/'
 ){
 
   # Convert inputs into targets.
@@ -20,9 +31,18 @@ plan peadm::backup (
   $compiler_targets                 = peadm::get_targets($compiler_hosts)
   $primary_postgresql_target        = peadm::get_targets($primary_postgresql_host, 1)
 
+  # Ensure input valid for a supported architecture
+  $arch = peadm::assert_supported_architecture(
+    $primary_host,
+    $replica_host,
+    $primary_postgresql_host,
+    $replica_postgresql_host,
+    $compiler_hosts,
+  )
+
   if $backup_classification {
     out::message('# Backing up classification')
-    run_task('peadm::backup_classification', $primary_target,
+    run_task('peadm::backup_classification', $primary_host,
     directory => '$backup_directory',
     )
   }
