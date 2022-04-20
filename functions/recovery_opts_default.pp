9115f6fa0b1a17a871059bd60bdb7df83a740d2e
Reid Vandewiele <reid@puppet.com>
2022-04-20 09:49:18 -0700

    Add recovery_opts type
    This type defines the inputs for the recovery operations backup and
    restore.

---
+++ functions/recovery_opts_default.pp
@@ -0,0 +1,10 @@
+function peadm::recovery_opts_default () {
+  {
+    'orchestrator' => true,
+    'puppetdb'     => true,
+    'rbac'         => true,
+    'activity'     => true,
+    'ca'           => false,
+    'classifier'   => true,
+  }
+}
