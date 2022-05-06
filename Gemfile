724fcbeb46588cc05ce15d80202bfba31dba134e
Cody Herriges <193064+ody@users.noreply.github.com>
2022-05-06 18:51:35 +0000

    Repair failing add_replica tests after additions
    Changes to add_replica which fix classificaton invalidate tests,
    commit makes them valid again.

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ group :development do
   gem "puppet-module-win-dev-r#{minor_version}", '~> 1.0',       require: false, platforms: [:mswin, :mingw, :x64_mingw]
   gem "puppet-debugger", '>= 0.18.0',                            require: false
   gem "bolt", '>= 3.17.0',                                       require: false
-  gem "github_changelog_generator",                              require: false
+  gem "github_changelog_generator", '>= 1.16.4',                 require: false
   gem "octokit", '4.21.0',                                       require: false
 end
 group :system_tests do
--- spec/plans/add_replica_spec.rb
+++ spec/plans/add_replica_spec.rb
@@ -11,33 +11,39 @@ describe 'peadm::add_replica' do
 
   describe 'basic functionality' do
     let(:params) { { 'primary_host' => 'primary', 'replica_host' => 'replica' } }
-    let(:certdata) { { 'certname' => 'primary', 'extensions' => { '1.3.6.1.4.1.34380.1.1.9813' => 'A' } } }
+    let(:certdata) { { 'certname' => 'primary', 'extensions' => { '1.3.6.1.4.1.34380.1.1.9813' => 'A' }, 'dns-alt-names' => [] } }
+    let(:cfg) { { 'params' => { 'primary_host' => 'primary' } } }
 
-    it 'runs successfully when the primary doesn\'t have alt-names' do
+    it 'runs successfully when the primary does not have alt-names' do
       allow_standard_non_returning_calls
-      expect_task('peadm::cert_data').always_return(certdata)
+      expect_task('peadm::get_peadm_config').always_return(cfg)
+      expect_task('peadm::cert_data').always_return(certdata).be_called_times(3)
+      expect_task('package').always_return({ 'status' => 'uninstalled' })
       expect_task('peadm::agent_install')
         .with_params({ 'server'        => 'primary',
                        'install_flags' => [
+                         'main:dns_alt_names=replica',
                          '--puppet-service-ensure', 'stopped',
-                         'main:certname=replica',
-                         'main:dns_alt_names=replica'
+                         'main:certname=replica'
                        ] })
 
+      expect_out_message.with_params('Classification to be updated using the following hash...')
       expect(run_plan('peadm::add_replica', params)).to be_ok
     end
 
     it 'runs successfully when the primary has alt-names' do
       allow_standard_non_returning_calls
-      expect_task('peadm::cert_data').always_return(certdata.merge({ 'dns-alt-names' => ['primary', 'alt'] }))
+      expect_task('peadm::get_peadm_config').always_return(cfg)
+      expect_task('peadm::cert_data').always_return(certdata.merge({ 'dns-alt-names' => ['primary', 'alt'] })).be_called_times(3)
+      expect_task('package').always_return({ 'status' => 'uninstalled' })
       expect_task('peadm::agent_install')
         .with_params({ 'server'        => 'primary',
                        'install_flags' => [
+                         'main:dns_alt_names=replica,alt',
                          '--puppet-service-ensure', 'stopped',
-                         'main:certname=replica',
-                         'main:dns_alt_names=replica,alt'
+                         'main:certname=replica'
                        ] })
-
+      expect_out_message.with_params('Classification to be updated using the following hash...')
       expect(run_plan('peadm::add_replica', params)).to be_ok
     end
   end
