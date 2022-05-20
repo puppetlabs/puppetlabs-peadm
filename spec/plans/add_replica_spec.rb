8b678e44d60ab3a02b8154bde3c11a52f9f66a9a
Cody Herriges <193064+ody@users.noreply.github.com>
2022-05-20 20:11:49 +0000

    Fix tests after inclusion of cert_valid_status
    The addition of the peadm::cert_valid_status task triggered test suite
    failures. Commit fixes them.

--- spec/plans/add_replica_spec.rb
+++ spec/plans/add_replica_spec.rb
@@ -11,13 +11,23 @@ describe 'peadm::add_replica' do
 
   describe 'basic functionality' do
     let(:params) { { 'primary_host' => 'primary', 'replica_host' => 'replica' } }
-    let(:certdata) { { 'certname' => 'primary', 'extensions' => { '1.3.6.1.4.1.34380.1.1.9813' => 'A' }, 'dns-alt-names' => [] } }
+    let(:certdata) { {
+      'certificate-exists' => true,
+      'certname'           => 'primary',
+      'extensions'         => { '1.3.6.1.4.1.34380.1.1.9813' => 'A' },
+      'dns-alt-names'      => []
+    } }
+    let(:certstatus) { {
+      'certificate-status' => 'valid',
+      'reason'             => 'Expires - 2099-01-01 00:00:00 UTC'
+    } }
     let(:cfg) { { 'params' => { 'primary_host' => 'primary' } } }
 
     it 'runs successfully when the primary does not have alt-names' do
       allow_standard_non_returning_calls
       expect_task('peadm::get_peadm_config').always_return(cfg)
       expect_task('peadm::cert_data').always_return(certdata).be_called_times(4)
+      expect_task('peadm::cert_valid_status').always_return(certstatus)
       expect_task('package').always_return({ 'status' => 'uninstalled' })
       expect_task('peadm::agent_install')
         .with_params({ 'server'        => 'primary',
@@ -26,6 +36,7 @@ describe 'peadm::add_replica' do
                          '--puppet-service-ensure', 'stopped',
                          'main:certname=replica'
                        ] })
+      expect_plan('peadm::util::sync_global_hiera')
 
       expect_out_verbose.with_params('Current config is...')
       expect_out_verbose.with_params('Updating classification to...')
@@ -36,6 +47,7 @@ describe 'peadm::add_replica' do
       allow_standard_non_returning_calls
       expect_task('peadm::get_peadm_config').always_return(cfg)
       expect_task('peadm::cert_data').always_return(certdata.merge({ 'dns-alt-names' => ['primary', 'alt'] })).be_called_times(4)
+      expect_task('peadm::cert_valid_status').always_return(certstatus)
       expect_task('package').always_return({ 'status' => 'uninstalled' })
       expect_task('peadm::agent_install')
         .with_params({ 'server'        => 'primary',
@@ -44,6 +56,8 @@ describe 'peadm::add_replica' do
                          '--puppet-service-ensure', 'stopped',
                          'main:certname=replica'
                        ] })
+      expect_plan('peadm::util::sync_global_hiera')
+
       expect_out_verbose.with_params('Current config is...')
       expect_out_verbose.with_params('Updating classification to...')
       expect(run_plan('peadm::add_replica', params)).to be_ok
