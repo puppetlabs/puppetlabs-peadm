f5ae8a816c586d0a3b404a86505d4b451a7a7139
Cody Herriges <193064+ody@users.noreply.github.com>
2022-06-16 22:22:16 +0000

    Fix tests for add_compiler
    Introduced new things that needed to be mocked to finish successfully

--- spec/plans/add_compiler_spec.rb
+++ spec/plans/add_compiler_spec.rb
@@ -19,10 +19,24 @@ describe 'peadm::add_compiler' do
       }
     end
 
+    let(:cfg) do
+      {
+        'params' => {
+          'primary_host' => 'primary'
+        },
+        'role-letter' => {
+          'server' => {
+            'A' => 'server_a',
+            'B' => 'server_b'
+          }
+        }
+      }
+    end
     let(:certdata) { { 'certname' => 'primary', 'extensions' => { '1.3.6.1.4.1.34380.1.1.9813' => 'A' } } }
 
     it 'runs successfully when no alt-names are specified' do
       allow_standard_non_returning_calls
+      expect_task('peadm::get_peadm_config').always_return(cfg)
       expect_plan('peadm::modify_certificate').always_return('mock' => 'mock')
       expect_task('peadm::agent_install')
         .with_params({ 'server'        => 'primary',
@@ -35,6 +49,7 @@ describe 'peadm::add_compiler' do
       #   ["--puppet-service-ensure", "stopped",
       #   "extension_requests:1.3.6.1.4.1.34380.1.3.13=pe_compiler", "extension_requests:1.3.6.1.4.1.34380.1.1.9813=A", "main:certname=compiler"], "server"=>"primary"}
 
+      expect_plan('peadm::util::sync_global_hiera').be_called_times(1)
       expect(run_plan('peadm::add_compiler', params)).to be_ok
     end
 
@@ -45,6 +60,7 @@ describe 'peadm::add_compiler' do
 
       it 'runs successfully when alt-names are specified' do
         allow_standard_non_returning_calls
+        expect_task('peadm::get_peadm_config').always_return(cfg)
         expect_plan('peadm::modify_certificate').always_return('mock' => 'mock')
         expect_task('peadm::agent_install')
           .with_params({ 'server'        => 'primary',
@@ -53,7 +69,7 @@ describe 'peadm::add_compiler' do
                            '--puppet-service-ensure', 'stopped',
                            'main:certname=compiler'
                          ] })
-
+        expect_plan('peadm::util::sync_global_hiera').be_called_times(1)
         expect(run_plan('peadm::add_compiler', params2)).to be_ok
       end
     end
