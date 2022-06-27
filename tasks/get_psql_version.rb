68192ebd5a5e10709fefb5649177ea5a9ec8c621
Cody Herriges <193064+ody@users.noreply.github.com>
2022-06-27 20:46:40 +0000

    Switch to ruby_task_helper
    It was determined that original pattern did not provide any additional
    value over more established ones.

--- tasks/get_psql_version.rb
+++ tasks/get_psql_version.rb
@@ -1,24 +1,15 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 # frozen_string_literal: true
 
+require_relative '../../ruby_task_helper/files/task_helper.rb'
 require '/opt/puppetlabs/puppet/cache/lib/pe_install/pe_postgresql_info.rb'
 
-# GetPEAdmConfig task class
-class GetPSQLInfo
-  def initialize(params); end
-
-  def execute!
+# Task which fetches the installed PSQL server major version
+class GetPSQLInfo < TaskHelper
+  def task(**_kwargs)
     psql_info = PEPostgresqlInfo.new
-    data = { 'version' => psql_info.installed_server_version }
-    puts data.to_json
+    { 'version' => psql_info.installed_server_version }
   end
 end
 
-# Run the task unless an environment flag has been set, signaling not to. The
-# environment flag is used to disable auto-execution and enable Ruby unit
-# testing of this task.
-unless ENV['RSPEC_UNIT_TEST_MODE']
-  Puppet.initialize_settings
-  task = GetPSQLInfo.new(JSON.parse(STDIN.read))
-  task.execute!
-end
+GetPSQLInfo.run if $PROGRAM_NAME == __FILE__
