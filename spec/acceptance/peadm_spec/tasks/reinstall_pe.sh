7355630562fc820d21aed627fae333d5462e8978
Dimitri Tischenko <dimitri@puppet.com>
2023-12-02 17:34:35 +0100

    add PE reinstall step

---
+++ spec/acceptance/peadm_spec/tasks/reinstall_pe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+# This task reinstalls PE and needs to run as root.
+# It assumes el-8-x86_64
+
+# Uninstall PE if installed
+/opt/puppetlabs/bin/puppet-enterprise-uninstaller -p -d -y || true 
+
+# Download PE
+INSTALLER="puppet-enterprise-${PT_version}-${PT_arch}"
+curl -O "https://s3.amazonaws.com/pe-builds/released/${PT_version}/${INSTALLER}.tar.gz"
+tar xf "${INSTALLER}.tar.gz"
+
+# Install PE. We need to pass "y" through stdin since the flag -y requires pe.conf to be present.
+cd $INSTALLER
+echo 'y' | ./puppet-enterprise-installer
