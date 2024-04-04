e198fa4cb0779b9021a15c5565d1346b7845a090
Dimitri Tischenko <dimitri.tischenko@perforce.com>
2024-04-04 16:49:46 +0200

    removing obsolete files

--- spec/acceptance/peadm_spec/plans/dbttest.pp
+++
@@ -1,9 +0,0 @@
-plan peadm_spec::dbttest(
-  TargetSpec $targets,
-) {
-  $t = get_target($targets)
-  out::message('Running dbt test')
-  out::message("targets: ${t}")
-  out::message("config: ${t.config}")
-  out::message("config: ${t.config['ssh']['user']}")
-}
