341cbc9561a7f5beb5600b55f925f8393fedcbd4
Jo Lillie <jo.lillie@puppet.com>
2024-04-11 15:32:19 +0100

    (PE-37236) Remove experimental status of add_compiler (#428)

--- plans/add_compiler.pp
+++ plans/add_compiler.pp
@@ -1,6 +1,4 @@
-# This plan is in development and currently considered experimental.
-#
-# @api private
+# @api public
 #
 # @summary Add a new compiler to a PE architecture or replace an existing one with new configuration.
 # @param avail_group_letter _ Either A or B; whichever of the two letter designations the compiler is being assigned to
