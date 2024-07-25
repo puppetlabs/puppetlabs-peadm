acfab3b1232229a49e8cc22acb829799e4afbcc8
Tim Meusel <tim@bastelfreak.de>
2024-07-25 11:17:59 +0200

    certname(): reenable test (#457)
    This was previously disabled because it didn't pass on travis. Let's see
    how it works on GitHub Actions.

--- spec/functions/certname_spec.rb
+++ spec/functions/certname_spec.rb
@@ -4,11 +4,14 @@ require 'spec_helper'
 
 describe 'peadm::certname' do
   include BoltSpec::BoltContext
-
+  around(:each) do |example|
+    in_bolt_context do
+      example.run
+    end
+  end
   let(:target) do
     ['test-vm.puppet.vm']
   end
 
-  # TODO: this *should* work, but is failing in TravisCI. Not sure why. It works on my laptop...
-  xit { in_bolt_context { is_expected.to run.with_params(target).and_return('test-vm.puppet.vm') } }
+  it { is_expected.to run.with_params(target).and_return('test-vm.puppet.vm') }
 end
