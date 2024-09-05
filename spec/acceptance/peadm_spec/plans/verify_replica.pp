78cb9e70daa2f280ef15a25d175625ddcc2833f5
Aaron Shannon <aaron.shannon@puppet.com>
2024-09-05 16:34:48 +0100

    PE-39228 Smoke test created and added to workflow

---
+++ spec/acceptance/peadm_spec/plans/verify_replica.pp
@@ -0,0 +1,20 @@
+plan peadm_spec::verify_replica() {
+  $t = get_targets('*')
+  wait_until_available($t)
+
+  $primary_host = $t.filter |$n| { $n.vars['role'] == 'primary' }
+
+if $primary_host == [] {
+  fail_plan('"primary" role missing from inventory, cannot continue')
+}
+
+$result = run_task('peadm::get_peadm_config', $primary_host, '_catch_errors' => true)
+
+$replica_host = $result.first['result']['params']['replica_host']
+
+if $replica_host == undef or $replica_host == null {
+  fail_plan("No replica was found in the PE configuration")
+} else {
+  out::message("Replica added successfully: ${replica_host}")
+}
+}
