a6023efd5f0a6a8941bf27021714c34799f01d73
Aaron Shannon <aaron.shannon@puppet.com>
2024-09-10 14:20:33 +0100

    PE-39228 whitespace fixed

--- spec/acceptance/peadm_spec/plans/verify_replica.pp
+++ spec/acceptance/peadm_spec/plans/verify_replica.pp
@@ -4,17 +4,17 @@ plan peadm_spec::verify_replica() {
 
   $primary_host = $t.filter |$n| { $n.vars['role'] == 'primary' }
 
-if $primary_host == [] {
-  fail_plan('"primary" role missing from inventory, cannot continue')
-}
+  if $primary_host == [] {
+    fail_plan('"primary" role missing from inventory, cannot continue')
+  }
 
-$result = run_task('peadm::get_peadm_config', $primary_host, '_catch_errors' => true).first.to_data()
+  $result = run_task('peadm::get_peadm_config', $primary_host, '_catch_errors' => true).first.to_data()
 
-$replica_host = $result['value']['params']['replica_host']
+  $replica_host = $result['value']['params']['replica_host']
 
-if $replica_host == undef or $replica_host == null {
-  fail_plan("No replica was found in the PE configuration")
-} else {
-  out::message("Replica added successfully: ${replica_host}")
-}
+  if $replica_host == undef or $replica_host == null {
+    fail_plan("No replica was found in the PE configuration")
+  } else {
+    out::message("Replica added successfully: ${replica_host}")
+  }
 }
