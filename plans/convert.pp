bb0f87cc8ded4d581df0371bfafa75605bf30d9e
Jacob Buchanan <jacob.buchanan@perforce.com>
2024-10-14 12:40:38 +0100

    (PE-39429) Simplify targets for restarting compiler services

--- plans/convert.pp
+++ plans/convert.pp
@@ -305,12 +305,10 @@ plan peadm::convert (
     # Restart cluster compiler services that are likely not restarted by our
     # final Puppet run to increase chance everything is functional upon plan
     # completion
-    run_command('systemctl restart pe-puppetserver.service',
-    $all_targets - $primary_target - $primary_postgresql_target - $replica_postgresql_target)
+    run_command('systemctl restart pe-puppetserver.service', $legacy_compiler_targets)
 
     # PuppetDB is only found on modern compilers, not legacy ones
-    run_command('systemctl restart pe-puppetdb.service',
-    $all_targets - $primary_target - $primary_postgresql_target - $replica_postgresql_target - $legacy_compiler_targets)
+    run_command('systemctl restart pe-puppetdb.service pe-puppetserver.service', $compiler_hosts)
 
     # Run puppet on all targets again to ensure everything is fully up-to-date
     run_task('peadm::puppet_runonce', $all_targets)
