b07439aa3f86028e8e1f74d19657b1faa7fc5814
Jacob Buchanan <jbuchanan21@qub.ac.uk>
2024-10-17 11:36:19 +0100

    (PE-39429) Only restart puppetdb on non-legacy compilers (#511)
    * (PE-39429) Only restart puppetdb on non-legacy compilers
    
    * (PE-39429) Simplify targets for restarting compiler services
    
    * (maint) Fix pdk lint error
    
    * (PE-39429) Use correct targets and only run if those targets are non-empty. This fixes errors about run_command needing 3 args when no compilers are present

--- types/convertsteps.pp
+++ types/convertsteps.pp
@@ -1,4 +1,4 @@
 #
 # @summary type for the different steps where the peadm::convert plan can be started
 #
-type Peadm::ConvertSteps = Enum[ 'modify-primary-certs', 'modify-infra-certs', 'convert-node-groups', 'finalize']
+type Peadm::ConvertSteps = Enum['modify-primary-certs', 'modify-infra-certs', 'convert-node-groups', 'finalize']
