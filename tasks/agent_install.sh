c935299aad89f62c5c98a3526970202794b0188e
Kaustubh Badrike <24961562+LordKa0S@users.noreply.github.com>
2024-10-23 10:00:12 -0700

    fix: echo environment variable containing special char (#516)

--- tasks/agent_install.sh
+++ tasks/agent_install.sh
@@ -6,6 +6,6 @@ if [ -x "/opt/puppetlabs/bin/puppet" ]; then
   echo "WARNING: Puppet agent is already installed. Re-install, re-configuration, or upgrade not supported and might fail."
 fi
 
-flags=$(echo $PT_install_flags | sed -e 's/^\["*//' -e 's/"*\]$//' -e 's/", *"/ /g')
+flags=$(echo "$PT_install_flags" | sed -e 's/^\["*//' -e 's/"*\]$//' -e 's/", *"/ /g')
 
 curl -k "https://${PT_server}:8140/packages/current/install.bash" | bash -s -- $flags
