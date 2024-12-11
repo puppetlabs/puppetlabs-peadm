116283e33a69a54e7bcf51298a5f3702f40a3d33
Jacob Buchanan <jacob.buchanan@perforce.com>
2024-12-11 16:30:04 +0000

    (maint) Make PE 2023 install by default

--- spec/plans/subplans/install_spec.rb
+++ spec/plans/subplans/install_spec.rb
@@ -87,11 +87,11 @@ describe 'peadm::subplans::install' do
     expect(run_plan('peadm::subplans::install', params)).to be_ok
   end
 
-  it 'installs 2021.7.9 with legacy compilers' do
+  it 'installs 2023.8.0 with legacy compilers' do
     params = {
       'primary_host' => 'primary',
       'console_password' => 'puppetLabs123!',
-      'version' => '2021.7.9',
+      'version' => '2023.8.0',
       'legacy_compilers' => ['compiler1', 'compiler2'],
     }
     expect(run_plan('peadm::subplans::install', params)).to be_ok
