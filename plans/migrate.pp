9b466a368ce3f6bf4d028e3a6ceb3cc83076a59e
petergmurphy <petermurphy@perforce.com>
2025-02-20 17:34:52 +0000

    (PE-40371) Add migration plan for standard architecture
    This commit adds a migration plan which utilises backup, install and
    restore plans to install and restore PE on new infrastructure with a
    standard architecture.
    
    This commit also introduces a new 'migration' option for backup and
    restore plans, enabling a new migration plan.

---
+++ plans/migrate.pp
@@ -0,0 +1,43 @@
+# @summary Migrate a PE primary server to a new host
+#
+# @param old_primary_host
+#   The existing PE primary server that will be migrated from
+# @param new_primary_host
+#   The new server that will become the PE primary server
+#
+plan peadm::migrate (
+  Peadm::SingleTargetSpec $old_primary_host,
+  Peadm::SingleTargetSpec $new_primary_host,
+) {
+  peadm::assert_supported_bolt_version()
+
+  $backup_file = run_plan('peadm::backup', $old_primary_host, {
+      backup_type => 'migration',
+  })
+
+  download_file($backup_file['path'], 'backup', $old_primary_host)
+
+  $backup_filename = basename($backup_file['path'])
+  $remote_backup_path = "/tmp/${backup_filename}"
+  $current_dir = system::env('PWD')
+
+  upload_file("${current_dir}/downloads/backup/${old_primary_host}/${backup_filename}", $remote_backup_path, $new_primary_host)
+
+  $old_primary_target = get_targets($old_primary_host)[0]
+  $old_primary_password = peadm::get_pe_conf($old_primary_target)['console_admin_password']
+  $old_pe_conf = run_task('peadm::get_peadm_config', $old_primary_target).first.value
+
+  run_plan('peadm::install', {
+      primary_host                => $new_primary_host,
+      console_password            => $old_primary_password,
+      code_manager_auto_configure => true,
+      download_mode               => 'direct',
+      version                     => $old_pe_conf['pe_version'],
+  })
+
+  run_plan('peadm::restore', {
+      targets => $new_primary_host,
+      restore_type => 'migration',
+      input_file => $remote_backup_path,
+  })
+}
