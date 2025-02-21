c8375358d39dbd4ee603ebad4b40db0900e7d7c1
David Mallon <david.mallon@perforce.com>
2025-02-21 09:17:24 +0000

    fix logic

--- spec/acceptance/peadm_spec/plans/test_replace_failed_postgres.pp
+++ spec/acceptance/peadm_spec/plans/test_replace_failed_postgres.pp
@@ -32,14 +32,17 @@ plan peadm_spec::test_replace_failed_postgres(
 
   out::message("Primary PostgreSQL host: ${primary_postgres_host}")
   out::message("Replica PostgreSQL host: ${replica_postgres_host}")
+  out::message("working_postgresql_host: ${working_postgresql_host}")
+  out::message("failed_postgresql_host: ${failed_postgresql_host}")
+  out::message("replacement_postgresql_host: ${replacement_postgresql_host}")
   if $primary_postgres_host == $failed_postgresql_host or $replica_postgres_host == $failed_postgresql_host {
     fail_plan("Failed PostgreSQL host ${failed_postgresql_host} was not removed from the PE configuration")
   } else {
     out::message("Failed PostgreSQL host ${failed_postgresql_host} was removed from the PE configuration")
   }
   if $primary_postgres_host == $replacement_postgresql_host or $replica_postgres_host == $replacement_postgresql_host {
-    fail_plan("Replacement PostgreSQL host ${replacement_postgresql_host} was not added the PE configuration")
-  } else {
     out::message("Replacement PostgreSQL host ${replacement_postgresql_host} was added to the PE configuration")
+  } else {
+    fail_plan("Replacement PostgreSQL host ${replacement_postgresql_host} was not added the PE configuration")
   }
 }
