08eeb1aaaf47bc443995797a3eba95096ff8c4b4
David Mallon <david.mallon@perforce.com>
2025-02-24 15:10:20 +0000

    (PE-40377) move install step to correct position again

--- spec/acceptance/peadm_spec/plans/provision_test_cluster.pp
+++ spec/acceptance/peadm_spec/plans/provision_test_cluster.pp
@@ -67,7 +67,7 @@ plan peadm_spec::provision_test_cluster (
         ['primary', 'compiler', 'replica', 'compiler', 'new-primary', 'new-replica']
       }
       'extra-large-migration': {
-        ['primary', 'primary-pdb-postgresql', 'compiler']
+        ['primary', 'primary-pdb-postgresql', 'compiler', 'new-primary', 'new-primary-pdb-postgresql']
       }
       'extra-large-with-dr-migration': {
         ['primary', 'primary-pdb-postgresql', 'compiler', 'replica', 'replica-pdb-postgresql', 'compiler', 'new-primary', 'new-replica', 'new-primary-pdb-postgresql', 'new-replica-pdb-postgresql']
