4e12a6ebb3486d5def44dcfd943cd6a43cc50052
Charitha Dunuwille <charitha.dunuwille@perforce.com>
2026-09-08 15:22:46 +0100

    (PE-46426) Add specs for two Puppet function files
    node_manager_yaml_location.rb and module_version.rb had no spec at all
    (unlike bolt_version.rb, which already has one). Both new specs note
    that they add real regression coverage but won't move the SimpleCov
    percentage, per the attribution issue documented in the next commit.

---
+++ spec/functions/node_manager_yaml_location_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+# This function is loaded through Puppet's function loader
+# (Puppet::Pops::Loader), not Ruby's `require` -- per
+# documentation/test-coverage.md's SimpleCov section, this means
+# SimpleCov/Ruby's Coverage module can never attribute this file's
+# execution back to lib/puppet/functions/peadm/node_manager_yaml_location.rb.
+# This spec still adds real regression coverage (it would catch a mutation
+# to the join logic or filename), it just won't move the SimpleCov
+# percentage.
+describe 'peadm::node_manager_yaml_location' do
+  # Catches a mutation that joins the wrong setting (e.g. :vardir instead
+  # of :confdir) -- the expected value is computed independently here so
+  # the assertion doesn't depend on (or go stale with) the test harness's
+  # actual confdir default.
+  it 'returns node_manager.yaml joined onto the real confdir setting' do
+    expected = File.join(Puppet.settings['confdir'], 'node_manager.yaml')
+    is_expected.to run.and_return(expected)
+  end
+
+  # Catches a mutation that hardcodes a different filename (e.g.
+  # 'node-manager.yaml' or drops the .yaml extension).
+  it 'returns node_manager.yaml joined onto a stubbed confdir' do
+    Puppet[:confdir] = '/etc/puppetlabs/puppet'
+    is_expected.to run.and_return('/etc/puppetlabs/puppet/node_manager.yaml')
+  end
+end
